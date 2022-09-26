7517ef6ae01d7acf27daddfcc1154c13bab39fb4
strXnger <44970984+strXnger@users.noreply.github.com>
2022-09-26 20:07:06 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,38 +1,26 @@
 #include "main.h"
-
 /**
- * _strstr - locates a substring
- * @haystack: string to search through
- * @needle: string to match
- * Return: pointer to initial segment of haystack
- */
-
+ * _strstr - locating a substring
+ * @needle: occurance to find
+ * @haystack: string to search
+ * Return: pointer
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, start;
-	int nlen = 0;
 
-	i = 0;
-	j = 0;
-	start = 0;
+	while (*haystack)
+	{
+	char *src = haystack;
+	char *sub = needle;
 
-	while (needle[nlen] != '\0')
+	while (*haystack && *sub && *haystack == *sub)
 	{
-		nlen++;
+		haystack++;
+		sub++;
 	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
-		{
-			if (j == 0)
-			{
-				start = i;
-			}
-			if (j == nlen - 1)
-			{
-				return (haystack + start);
-			}
-		}
+	if (!*sub)
+	return (src);
+	haystack = src + 1;
 	}
-	return (0);
+return (0);
 }
